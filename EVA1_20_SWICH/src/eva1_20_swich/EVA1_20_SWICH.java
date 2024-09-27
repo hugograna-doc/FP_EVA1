7a1deb76d55f67bc8c4c0dd0b8352022fd0031d8
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-09-27 15:10:40 -0600

    Add files via upload

---
+++ EVA1_20_SWICH/src/eva1_20_swich/EVA1_20_SWICH.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_20_swich;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_20_SWICH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int dia;
+        Scanner captu = new Scanner (System.in);
+        System.out.println("Número correspondiente al dia (1 - 7):");
+        dia = captu.nextInt();
+        
+        switch(dia){
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;//ultima instrucción - FIN DEL CASO
+            default://OPCIONAL, SIN BREAK, ULTIMA SECCIÓN DEL SWITCH
+                System.out.println("Número no válido (Deben ser del 1 al 7) ");
+                
+                
+                
+                
+        }
+    }
+    
+}
