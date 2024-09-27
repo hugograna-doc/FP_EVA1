a0372d6ff8c6d6694c1b9d062c7da76927cca63f
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-09-27 15:08:33 -0600

    Add files via upload

---
+++ EVA1_13_AGUILA_SOL/src/eva1_13_aguila_sol/EVA1_13_AGUILA_SOL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_13_aguila_sol;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_13_AGUILA_SOL {
+    private static boolean Aguila;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ALEATORIO
+        //GENERAR NÚMEROS ALEATORIOS
+        double valor = Math.random();
+        //random genera valores >=o y < a 1
+        //entre 0 y .9999999999999
+        if(valor < .5)
+            System.out.println(Aguila);
+        else
+            
+            
+             System.out.println("Sol");
+            
+            
+    }
+    
+}
