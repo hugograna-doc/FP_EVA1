b2bc2a24f6a548cacf840d27c302f5115b7c2b9e
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-10-23 21:37:21 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author Hugo
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+         
+        // DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "FORD";
+        modelo = "MUSTANG";
+        year = 2022;
+        precio = 9999999.111;
+        
+        System.out.println(marca);//imprime el contenido
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+
+    }
+}
